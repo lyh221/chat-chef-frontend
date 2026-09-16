b773d874f05b58779c7dbeac6f4754c258ba2843
lyh221 <lyh221@naver.com>
2026-09-16 17:51:18 +0900

    완료

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -3,9 +3,12 @@ import PrevButton from "../components/PrevButton";
 import InfoInput from "../components/InfoInput";
 import AddButton from "../components/AddButton";
 import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 const Info = () => {
   // logic
+
+  const history = useNavigate()
 
   // TODO: set함수 추가하기
   const [ingredientList] = useState([]); // 사용자가 입력할 재료 목록
@@ -15,7 +18,8 @@ const Info = () => {
   };
 
   const handleNext = () => {
-    console.log("chat페이지로 이동");
+    //react-roucter-dom을 이용한 페이지 이동 
+    history("/chat")
   };
 
   // view
